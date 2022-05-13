a2dbe35fd16093e0ca5a7e3c0af229f972d6be92
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-05-13 14:30:18 +0200

    inserimento vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  
+    <HeaderNuovo />
+  
+</template>
+
+<script>
+import HeaderNuovo from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderNuovo,
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  
+  <div class="head">
+    <div class="sx">
+      logo
+    </div>
+    <div class="dx">
+      menu
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderNuovo",
+}
+</script>
+
+
+<style scoped lang="scss">
+
+.head{
+  height: 200px;
+  background-color: lightblue;
+}
+
+</style>
